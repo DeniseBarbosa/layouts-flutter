17c9c62c8fabcbf0d76ae5bd49e55d4cd1a2cd0e
DeniseBarbosa <deniseluvoir@gmail.com>
2023-02-11 06:58:52 -0300

    Carrinho parte 1

--- shopping/lib/pages/carrinho.page.dart
+++ shopping/lib/pages/carrinho.page.dart
@@ -9,8 +9,7 @@ class PaginaCarrinho extends StatelessWidget {
         children: [
           Expanded(
             child: Container(
-              color: Colors.red,
-              child: Text("Container 1"),
+              child: produtoLista(),
             ),
           ),
           Container(
@@ -22,4 +21,95 @@ class PaginaCarrinho extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget produtoLista() {
+  return ListView(
+    children: <Widget>[
+      produtoItem(),
+      produtoItem(),
+      produtoItem(),
+      produtoItem(),
+      produtoItem(),
+    ],
+  );
+}
+
+Widget produtoItem() {
+  return Container(
+    height: 120,
+    margin: EdgeInsets.all(5),
+    child: Row(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.all(10),
+          child: Image.asset(
+            "assets/images/product-1.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 10,
+          ),
+          child: Column(
+            crossAxisAlignment:
+                CrossAxisAlignment.start, //=> alinhar todos os itens a esquerda
+            children: [
+              Text("Título do produto"),
+              Text(
+                "\$200",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                //=> caixinha de add ou eliminar
+
+                height: 30,
+                width: 120,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        child: Text("+"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: Text("1"),
+                    ),
+                    Container(
+                      width: 40,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        child: Text("-"),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
